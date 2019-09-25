388b40b2902e1eb873dc92bf57b77281cd25ece7
tweeet111 <tweeet@freenet.de>
2019-09-25 23:43:03 +0200

    update openpos implementation

--- ib2Excel.py
+++ ib2Excel.py
@@ -1,213 +1,257 @@
 from datetime import datetime as dt
-
 import numpy as np
 import pandas as pd
 import xlwings as xw
 from ib_insync import *
 import sys, signal
 from setup import *
+import time
 
 
-def stream_data():
-    def signal_handler(signal, frame):
-        print("closing....")
-        ib.disconnect()
-        sys.exit(0)
+def signal_handler(signal, frame):
+	print("closing....")
+	ib.disconnect()
+	sys.exit(0)
 
-    def get_libor_func():
-        print('retrieving Libor rates....')
-        tables = pd.read_html("https://www.finanzen.net/zinsen/libor/usd")
-        df_lib = tables[0]
-        df_lib["Kurs"] = pd.to_numeric(df_lib["Kurs"]).div(1000000)
-        df_lib.reset_index(inplace=True, drop=True)
-        df_lib['Years'] = pd.Series([1 / 365, 7 / 365, 30 / 365, 60 / 365, 90 / 365, 180 / 365, 1],
-                                    index=range(7)).round(3)
-        # rfr_func = interp1d(df_lib['Years'], df_lib["Kurs"], fill_value="extrapolate" )
+def get_libor_func():
+	print('retrieving Libor rates....')
+	tables = pd.read_html("https://www.finanzen.net/zinsen/libor/usd")
+	df_lib = tables[0]
+	df_lib["Kurs"] = pd.to_numeric(df_lib["Kurs"]).div(1000000)
+	df_lib.reset_index(inplace=True, drop=True)
+	df_lib['Years'] = pd.Series([1 / 365, 7 / 365, 30 / 365, 60 / 365, 90 / 365, 180 / 365, 1],
+								index=range(7)).round(3)
+	# rfr_func = interp1d(df_lib['Years'], df_lib["Kurs"], fill_value="extrapolate" )
 
-        # format df to fit export format
-        df_lib = df_lib[['Name', 'Kurs']]
-        df_lib = df_lib.pivot_table(columns='Name', index=None, values='Kurs', aggfunc='first') * 100
+	# format df to fit export format
+	df_lib = df_lib[['Name', 'Kurs']]
+	df_lib = df_lib.pivot_table(columns='Name', index=None, values='Kurs', aggfunc='first') * 100
 
-        cols = ["Date", "Libor USD Overnight", "Libor USD 1 Woche", "Libor USD 1 Monat", "Libor USD 2 Monate",
-                "Libor USD 3 Monate", "Libor USD 6 Monate", "Libor USD 12 Monate"]
+	cols = ["Date", "Libor USD Overnight", "Libor USD 1 Woche", "Libor USD 1 Monat", "Libor USD 2 Monate",
+			"Libor USD 3 Monate", "Libor USD 6 Monate", "Libor USD 12 Monate"]
 
-        df_lib['Date'] = dt.today().date().strftime('%d.%m.%y')
-        df_lib = df_lib[cols]
-        return df_lib
+	df_lib['Date'] = dt.today().date().strftime('%d.%m.%y')
+	df_lib = df_lib[cols]
+	return df_lib
 
-    def conn_tws(client=np.random.randint(0, 10)):
-        i = 0
-        while ib.isConnected() == False and i < 20:
-            try:
-                print("connecting...")
-                ib.connect('127.0.0.1', tws_port, clientId=client, timeout=None)  # tws 7496 gateway 4001 (see TWS settings)
-                ib.sleep(1)
-            except ConnectionRefusedError:
-                ib.sleep(3)
-                i += 1
-        if ib.isConnected():
-            print("Connection established")
-        else:
-            print("Timeout")
+def conn_tws(client=np.random.randint(0, 10)):
+	i = 0
+	while ib.isConnected() == False and i < 20:
+		try:
+			print("connecting...")
+			ib.connect('127.0.0.1', tws_port, clientId=client, timeout=None)  # tws 7496 gateway 4001 (see TWS settings)
+			ib.sleep(1)
+		except ConnectionRefusedError:
+			ib.sleep(3)
+			i += 1
+	if ib.isConnected():
+		print("Connection established")
+	else:
+		print("Timeout")
 
-    def get_tickers(min_dte=30, max_dte=250, strike_distance=25, strike_range=0.5):
-        ib.reqMarketDataType(marketDataType=2)
-        print('getting SPX...')
-        spx = Index('SPX', 'CBOE')
-        ib.qualifyContracts(spx)
-        print('getting chains...')
-        chains = ib.reqSecDefOptParams(spx.symbol, '', spx.secType, spx.conId)
-        chain_spx = next(c for c in chains if (c.tradingClass == 'SPX' and c.exchange == 'SMART'))
-        chain_spxw = next(c for c in chains if (c.tradingClass == 'SPXW' and c.exchange == 'SMART'))
+def get_tickers(min_dte=30, max_dte=250, strike_distance=25, strike_range=0.5):
+	ib.reqMarketDataType(marketDataType=2)
+	print('getting SPX...')
+	spx = Index('SPX', 'CBOE')
+	ib.sleep(1)
+	ib.qualifyContracts(spx)
+	ib.sleep(1)
+	print('getting chains...')
+	chains = ib.reqSecDefOptParams(spx.symbol, '', spx.secType, spx.conId)
+	chain_spx = next(c for c in chains if (c.tradingClass == 'SPX' and c.exchange == 'SMART'))
+	chain_spxw = next(c for c in chains if (c.tradingClass == 'SPXW' and c.exchange == 'SMART'))
 
-        print('getting SPX live...')
-        #spx_ticker = ib.reqMktData(spx, '', False, False)
-        spx_ticker = ib.reqTickers(spx)
-        ib.sleep(1)
-        #spxValue = spx_ticker.marketPrice()
-        spxValue = spx_ticker[0].last
-        print(spxValue)
-        ib.sleep(1)
+	print('getting SPX live...')
+	#spx_ticker = ib.reqMktData(spx, '', False, False)
+	spx_ticker = ib.reqTickers(spx)
+	ib.sleep(1)
+	#spxValue = spx_ticker[0].marketPrice()
+	spxValue = spx_ticker[0].last
+	print(spxValue)
+	ib.sleep(1)
 
-        print('combining SPX/SPXW expirations...')
-        strikes = [strike for strike in chain_spx.strikes
-                   if strike % strike_distance == 0
-                   and spxValue - strike_range * spxValue < strike < spxValue + strike_range * spxValue]
+	print('combining SPX/SPXW expirations...')
+	strikes = [strike for strike in chain_spx.strikes
+			   if strike % strike_distance == 0
+			   and spxValue - strike_range * spxValue < strike < spxValue + strike_range * spxValue]
 
-        expirations_spx = sorted(exp for exp in chain_spx.expirations)
-        # print(expirations_spx)
-        expirations_spxw = sorted(exp for exp in chain_spxw.expirations)
-        # print (expirations_spxw)
+	expirations_spx = sorted(exp for exp in chain_spx.expirations)
+	# print(expirations_spx)
+	expirations_spxw = sorted(exp for exp in chain_spxw.expirations)
+	# print (expirations_spxw)
 
-        expirations = expirations_spx + expirations_spxw
-        print(expirations)
+	expirations = expirations_spx + expirations_spxw
+	print(expirations)
 
-        print('filtering for min and max dte...')
+	print('filtering for min and max dte...')
 
-        dt_dates = [dt.strptime(date, '%Y%m%d') for date in expirations]
-        dt_dates = [date for date in dt_dates
-                    if max_dte > (date - dt.today()).days > min_dte]
-        expirations = [d.strftime('%Y%m%d') for d in dt_dates]
-        print(expirations)
+	dt_dates = [dt.strptime(date, '%Y%m%d') for date in expirations]
+	dt_dates = [date for date in dt_dates
+				if max_dte > (date - dt.today()).days > min_dte]
+	expirations = [d.strftime('%Y%m%d') for d in dt_dates]
+	print(expirations)
 
-        # alle Chain Contracts
-        print('finding contracts...')
-        contracts = [Option('SPX', exp, strike, right, 'SMART')
-                     for strike in strikes
-                     # for tradingClass in ['SPX','SPXW']
-                     for right in ['P', 'C']
-                     for exp in expirations
-                     ]
+	# alle Chain Contracts
+	print('finding contracts...')
+	contracts = [Option('SPX', exp, strike, right, 'SMART')
+				 for strike in strikes
+				 # for tradingClass in ['SPX','SPXW']
+				 for right in ['P', 'C']
+				 for exp in expirations
+				 ]
 
-        startTime = dt.now()
+	startTime = dt.now()
 
-        ib.qualifyContracts(*contracts)
-        contracts = [c for c in contracts if c.conId]
-        contracts = list(set(contracts))
-        ib.qualifyContracts(*contracts)
-        print('number of contracts: ' + str(len(contracts)))
-        setupTime = dt.now() - startTime
-        print('finished finding contracts in ' + str(setupTime) + 's')
+	ib.qualifyContracts(*contracts)
+	contracts = [c for c in contracts if c.conId]
+	contracts = list(set(contracts))
+	ib.qualifyContracts(*contracts)
+	print('number of contracts: ' + str(len(contracts)))
+	setupTime = dt.now() - startTime
+	print('finished finding contracts in ' + str(setupTime) + 's')
 
-        print('setting up ticker...')
-        startTime = dt.now()
-        #tickers = ib.reqTickers(*contracts)
-        setupTime = dt.now() - startTime
-        print('finished setting up ticker in ' + str(setupTime) + 's')
-        print('Startup finished!')
-        return contracts, spx
+	print('setting up ticker...')
+	startTime = dt.now()
+	#tickers = ib.reqTickers(*contracts)
+	setupTime = dt.now() - startTime
+	print('finished setting up ticker in ' + str(setupTime) + 's')
+	print('Startup finished!')
+	return contracts, spx
 
-    def update_price(contracts, spx):
+def update_price(contracts, spx):
 
-        now = dt.now()
-        #spxValue = spx_ticker.marketPrice()
-        spx_ticker = ib.reqTickers(spx)
-        spxValue = spx_ticker[0].last
+	now = dt.now()
 
-
-        print('updating...')
-        tickers = ib.reqTickers(*contracts)
-
-        df = pd.DataFrame(columns='STRIKE RIGHT EXPIRATION SYMBOL bid ask'.split())
-
-        df['STRIKE'] = [c.strike for c in contracts]
-        df['RIGHT'] = [c.right for c in contracts]
-        df['EXPIRATION'] = [c.lastTradeDateOrContractMonth for c in contracts]
-        df['SYMBOL'] = [c.symbol for c in contracts]
-        contract2Row = {c: i for (i, c) in enumerate(contracts)}
-        df['STRIKE'] = df['STRIKE'].astype(int)
-
-        for t in tickers:
-            row = contract2Row[t.contract]
-            df.iloc[row, 4:] = (t.bid, t.ask)
-
-        df['mid'] = (df['bid'] + df['ask']) / 2
-        df['OPTION_REF'] = df['SYMBOL'].str.ljust(6) + df.EXPIRATION
-        df['R_REF'] = [c.localSymbol for c in contracts]
-        df['UNDLY_PRICE'] = spxValue
-        df['TRADE_DT'] = now.strftime("%Y%m%d")
-        df['TRADE_TIME'] = now.strftime("%H:%M:%S")
-
-        print("Timestamp of latest ticker update: " + str(now.strftime("%H:%M:%S")))
-        df_calls = df[df['RIGHT'] == 'C']
-        df_puts = df[df['RIGHT'] == 'P']
-        df_puts = df_puts.rename(columns={'R_REF': 'PUT_REF'})
-
-        df_calls = df_calls[['STRIKE', 'R_REF', 'mid', 'OPTION_REF']]
-        df_calls = df_calls.rename(columns={'mid': 'CALL_MID'})
-        df_calls = df_calls.rename(columns={'R_REF': 'CALL_REF'})
-
-        df_exp = df_puts.merge(df_calls, on=["OPTION_REF", "STRIKE"], how='left')
-        df_exp['CALL_MID'] = pd.to_numeric(df_exp['CALL_MID'])
-        df_exp = df_exp.rename(columns={'mid': 'PUT_MID'})
-        df_exp = df_exp.rename(columns={'SYMBOL': 'UNDLY'})
-        df_exp = df_exp.drop(['bid', 'ask'], axis=1)
-        df_exp = df_exp.sort_values(['TRADE_DT', 'TRADE_TIME', 'OPTION_REF', 'STRIKE'], ascending=False)
-        df_exp.reset_index(drop=True, inplace=True)
-
-        cols = ["TRADE_DT", "TRADE_TIME", "UNDLY", "UNDLY_PRICE", "OPTION_REF", "STRIKE", "CALL_REF", "CALL_MID",
-                "PUT_REF", "PUT_MID"]
-        df_exp = df_exp[cols]
-        # df_exp = df_exp.query("CALL_MID>=0 & PUT_MID>=0" )  # delete negative prices bad data
-        updateTime = dt.now() - now
-        print('finished updating ticker in ' + str(updateTime) + 's')
-        print(updateTime)
-
-        return df_exp
-
-    def start_streaming(contracts, spx, wait_sec=0.1):
-        # timeout = time.time() + 30  # timeout loop after 30s
-        # while time.time() < timeout:
-        print('starting live data stream to Excel...every ' + str(wait_sec) + "seconds")
-        while True:
-            ib.sleep(wait_sec)
-            #     ib.pendingTickersEvent(tickers)
-            #     ib.sleep(10)
-            df = update_price(contracts, spx)
-            sht1.range('A1').options(index=False).value = df
+	spx_ticker = ib.reqTickers(spx)
+	#spxValue = spx_ticker[0].marketPrice()
+	spxValue = spx_ticker[0].last
 
 
-    signal.signal(signal.SIGINT, signal_handler)
-# create ib_insync IB() instance and connect to TWS
-    ib = IB()
-    ib.sleep(0.1)
-    conn_tws()
-    ib.sleep(0.1)
-    ib.client.MaxRequests = 48
+	print('updating Data tab...')
+	tickers = ib.reqTickers(*contracts)
+	ib.sleep(2)
 
-    df_lib = get_libor_func()
-    print('Libor download finished')
+	df = pd.DataFrame(columns='STRIKE RIGHT EXPIRATION SYMBOL bid ask'.split())
 
-    contracts, spx = get_tickers(min_dte, max_dte, strike_distance, strike_range)
-    print('connecting to Excel workbook...')
-    wb = xw.Book(workbook)
-    sht1 = wb.sheets[stream2tab]
-    sht2 = wb.sheets['RFRATE']
-    print('streaming Libor data to Excel...')
-    sht2.range('c1').options(index=False).value = df_lib
+	df['STRIKE'] = [c.strike for c in contracts]
+	df['RIGHT'] = [c.right for c in contracts]
+	df['EXPIRATION'] = [c.lastTradeDateOrContractMonth for c in contracts]
+	df['SYMBOL'] = [c.symbol for c in contracts]
+	contract2Row = {c: i for (i, c) in enumerate(contracts)}
+	df['STRIKE'] = df['STRIKE'].astype(int)
 
-    start_streaming(contracts, spx, wait_sec)
+	for t in tickers:
+		row = contract2Row[t.contract]
+		df.iloc[row, 4:] = (t.bid, t.ask)
+
+	df['mid'] = (df['bid'] + df['ask']) / 2
+	df['OPTION_REF'] = df['SYMBOL'].str.ljust(6) + df.EXPIRATION
+	df['R_REF'] = [c.localSymbol for c in contracts]
+	df['UNDLY_PRICE'] = spxValue
+	df['TRADE_DT'] = now.strftime("%Y%m%d")
+	df['TRADE_TIME'] = now.strftime("%H:%M:%S")
+
+	print("Timestamp of latest ticker update: " + str(now.strftime("%H:%M:%S")))
+	df_calls = df[df['RIGHT'] == 'C']
+	df_puts = df[df['RIGHT'] == 'P']
+	df_puts = df_puts.rename(columns={'R_REF': 'PUT_REF'})
+
+	df_calls = df_calls[['STRIKE', 'R_REF', 'mid', 'OPTION_REF']]
+	df_calls = df_calls.rename(columns={'mid': 'CALL_MID'})
+	df_calls = df_calls.rename(columns={'R_REF': 'CALL_REF'})
+
+	df_exp = df_puts.merge(df_calls, on=["OPTION_REF", "STRIKE"], how='left')
+	df_exp['CALL_MID'] = pd.to_numeric(df_exp['CALL_MID'])
+	df_exp = df_exp.rename(columns={'mid': 'PUT_MID'})
+	df_exp = df_exp.rename(columns={'SYMBOL': 'UNDLY'})
+	df_exp = df_exp.drop(['bid', 'ask'], axis=1)
+	df_exp = df_exp.sort_values(['TRADE_DT', 'TRADE_TIME', 'OPTION_REF', 'STRIKE'], ascending=False)
+	df_exp.reset_index(drop=True, inplace=True)
+
+	cols = ["TRADE_DT", "TRADE_TIME", "UNDLY", "UNDLY_PRICE", "OPTION_REF", "STRIKE", "CALL_REF", "CALL_MID",
+			"PUT_REF", "PUT_MID"]
+	df_exp = df_exp[cols]
+	# df_exp = df_exp.query("CALL_MID>=0 & PUT_MID>=0" )  # delete negative prices bad data
+	updateTime = dt.now() - now
+	print('finished updating data tab in ' + str(updateTime) + 's')
+
+	return df_exp
+
+def start_streaming(contracts, spx):
+	df = update_price(contracts, spx)
+	sht1.range('A1').options(index=False).value = df
+
+def update_ticks(spx):
+	spx_ticker = ib.reqTickers(spx)
+	spxValue = spx_ticker[0].last
+	#spxValue = spx_ticker[0].marketPrice()
+
+	# grab contracts from OPENPOS sheet
+
+	print('updating ticks...')
+	cons = sht3.range('B2').expand('down')
+	open_cons = cons.value
+	open_cons = [i for i in open_cons if i != 0]
+	contracts = [Contract('SPX', secType="OPT", localSymbol=opra, exchange='SMART')
+				 for opra in open_cons]
+	ib.qualifyContracts(*contracts)
+	tickers = [ib.reqMktData(c, '', False, False) for c in contracts]
+	ib.sleep(1)
+
+	now = dt.now()
+	df = pd.DataFrame(columns='localSymbol bid ask'.split())
+	df['localSymbol'] = [c.localSymbol for c in contracts]
+	contract2Row = {c: i for (i, c) in enumerate(contracts)}
+
+	for t in tickers:
+		row = contract2Row[t.contract]
+		df.iloc[row, 1:] = (t.bid, t.ask)
+	df= df[["bid","ask"]]
+	df['mid'] = (df['bid'] + df['ask']) / 2
+
+	df['time'] = now.strftime("%H:%M:%S")
+
+	print("Timestamp of latest openpos update: " + str(now.strftime("%H:%M:%S")))
+	return df
+
+def start_ticks(spx):
+	df = update_ticks(spx)
+	sht3.range('C1').options(index=False).value = df
 
 
 if __name__ == "__main__":
-    stream_data()
+
+	signal.signal(signal.SIGINT, signal_handler)
+
+# create ib_insync IB() instance and connect to TWS
+	ib = IB()
+	ib.sleep(0.1)
+	conn_tws()
+	ib.sleep(0.1)
+	ib.client.MaxRequests = 48
+	ib.reqMarketDataType(marketDataType=2)
+
+	df_lib = get_libor_func()
+	print('Libor download finished')
+
+	contracts, spx = get_tickers(min_dte, max_dte, strike_distance, strike_range)
+	print('connecting to Excel workbook...')
+	wb = xw.Book(workbook)
+	sht1 = wb.sheets[stream2tab]
+	sht2 = wb.sheets['RFRATE']
+	sht3 = wb.sheets['OPENPOS']
+	print('streaming Libor data to Excel...')
+	sht2.range('c1').options(index=False).value = df_lib
+
+
+
+
+	print("starting data streaming...")
+	while True:
+		start_streaming(contracts, spx)
+		end = time.time() + wait_data
+		while time.time() < end:
+			ib.sleep(wait_openpos)
+			start_ticks(spx)
+
+
--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ tws_port = 7496
 min_dte=50        # chains below are not updated
 max_dte=220       # chains above are not updated
 strike_distance= 25 # only update strikes divisible by value (25, 10, 50)
-strike_range= 0.5   # only update strikes up or down from current SPX spot (e.g. -50% to + 50%)
+strike_range= 0.4   # only update strikes up or down from current SPX spot (e.g. -50% to + 50%)
 
 # update interval in seconds
-wait_sec = 0.1
+wait_openpos = 1
+wait_data = 120
 
 # Excel workbook name
 #workbook = 'Backtest_software_v0.069.xlsm'
-workbook = "live_example.xlsb"
+workbook = "live_example2.xlsb"
 stream2tab = "DATA" # "FM_FEED"
